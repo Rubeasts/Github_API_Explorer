7083d3f1b7f4d543871248c175ceb4e293bbc814
rjollet <renaudjollet@gmail.com>
2016-10-15 00:16:37 +0800

    refactoring github library
    Add a class API to the github library
    this class handle the http github api

---
+++ lib/github_api.rb
@@ -0,0 +1,62 @@
+require 'yaml'
+require 'http'
+require 'json'
+
+# Create the response dumps
+# Get ENDPOINT categories
+
+module  Github
+  # Main class to set up a Github User
+  class API
+    GITHUB_API_URL = 'https://api.github.com'.freeze
+
+    def initialize(username, token)
+      @username = username
+      @token = token
+    end
+
+    def github_api_get_http(url)
+      HTTP.basic_auth(user: @username, pass: @token).get(url)
+    end
+
+    def github_api_wait_cache(url)
+      response = github_api_get_http(url)
+      while response.headers['Status'].split(' ').first == '202'
+        sleep(2)
+        response = github_api_get_http(url)
+      end
+    end
+
+    def github_api_get(route)
+      url = GITHUB_API_URL + route
+      response = github_api_wait_cache(url)
+      JSON.parse(response.to_s)
+    end
+
+    def user_info(username)
+      route = '/users/' + username
+      github_api_get(route)
+    end
+
+    def user_followers(username)
+      route = '/users/' + username + '/followers'
+      github_api_get(route)
+    end
+
+    def user_following(username)
+      route = '/users/' + username + '/following'
+      github_api_get(route)
+    end
+
+    def user_repos(username)
+      route = '/users/' + username + '/repos'
+      github_api_get(route)
+    end
+
+    def repo_stat(full_name, stat)
+      route = '/repos/' + full_name + '/stats/' + stat
+      puts route
+      github_api_get(route)
+    end
+  end
+end
--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -1,36 +1,53 @@
-require	'minitest/autorun'
+require 'minitest/autorun'
 require 'minitest/rg'
 require 'yaml'
 
 require './lib/github_developer.rb'
-require './lib/github_repository.rb'
+require './lib/github_api'
+require './lib/github_repository'
 
 RESULTS = YAML.load(File.read('spec/fixtures/results.yml'))
-USERNAME = "rjollet"
-developer = Github::Developer.new(USERNAME)
+USERNAME = 'rjollet'.freeze
+CREDENTIALS = YAML.load(File.read('config/github_credential.yml'))
 
 describe 'Github specifications' do
+  before do
+    @github_api = Github::API.new(
+      CREDENTIALS[:username],
+      CREDENTIALS[:token]
+    )
+    @developer = Github::Developer.new(@github_api, USERNAME)
+  end
+
   it 'should be able to open a new Github Developer' do
-    developer.name.length.must_be :>, 0
+    @developer.name.length.must_be :>, 0
   end
 
   it 'should get the user id' do
-  	developer.id.must_be :>, 0
+    @developer.id.must_be :>, 0
   end
 
   it 'should get the number of public repos' do
-  	developer.public_repos.must_be :>, 0
+    @developer.public_repos.must_be :>, 0
   end
 
   it 'should get the followers' do
-  	developer.followers.length.must_be :>, 0
+    @developer.followers.length.must_be :>, 0
   end
 
   it 'should get the following' do
-    developer.following.length.must_be :>=, 0
+    @developer.following.length.must_be :>=, 0
   end
 
   it 'should get the repos' do
-    developer.repos.length.must_be :>, 0
+    @developer.repos.length.must_be :>, 0
+  end
+
+  it 'should get the repo should have a full name' do
+    @developer.repos.first.full_name.must_be_instance_of String
+  end
+
+  it 'should get the repo should have stats' do
+    @developer.repos.first.stats.wont_be_nil
   end
 end
