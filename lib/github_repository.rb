7ccf90aa3e216f33d5aa342974765e55cef28638
rjollet <renaudjollet@gmail.com>
2016-10-15 00:16:37 +0800

    refactoring github library
    Add a class API to the github library
    this class handle the http github api

--- lib/github_repository.rb
+++ lib/github_repository.rb
@@ -1,8 +1,5 @@
-require 'yaml'
-require 'http'
-
-# Create the response dumps
-# Get ENDPOINT categories
+require_relative 'github_api'
+require_relative 'github_developer'
 
 module  Github
   # Main class to set up a Github User
@@ -12,55 +9,37 @@ module  Github
                 :watchers_count, :has_issues, :has_downloads, :forks_count,
                 :open_issues_count, :forks, :open_issues, :watchers
 
-    def initialize( id, name, full_name, is_private, is_fork, created_at,
-                    updated_at, pushed_at, size, stargazers_count,
-                    watchers_count, has_issues, has_downloads,
-                    forks_count, open_issues_count, forks, open_issues,
-                    watchers)
-      @id = id,
-      @name = name,
-      @full_name = full_name,
-      @is_private = is_private,
-      @is_fork = is_fork,
-      @created_at = created_at,
-      @updated_at = updated_at,
-      @pushed_at = pushed_at,
-      @size = size,
-      @stargazers_count = stargazers_count,
-      @watchers_count = watchers_count,
-      @has_issues = has_issues,
-      @has_downloads = has_downloads,
-      @forks_count = forks_count,
-      @open_issues_count = open_issues_count,
-      @forks = forks,
-      @open_issues = open_issues,
-      @watchers = watchers
-    end
-
-    def github_api_get(route)
-      url = 'https://api.github.com' + route
-      github_credential = YAML.load(File.read('config/github_credential.yml'))
-      HTTP.basic_auth(:user => github_credential[:username], :pass => github_credential[:token]).get(url)
+    def initialize(github_api, repo)
+      @github_api = github_api
+      @id = repo['id']
+      @name = repo['name']
+      @full_name = repo['full_name']
+      @is_private = repo['is_private']
+      @is_fork = repo['is_fork']
+      @created_at = repo['created_at']
+      @updated_at = repo['updated_at']
+      @pushed_at = repo['pushed_at']
+      @size = repo['size']
+      @stargazers_count = repo['stargazers_count']
+      @watchers_count = repo['watchers_count']
+      @has_issues = repo['has_issues']
+      @has_downloads = repo['has_downloads']
+      @forks_count = repo['forks_count']
+      @open_issues_count = repo['open_issues_count']
+      @forks = repo['forks']
+      @open_issues = repo['open_issues']
+      @watchers = repo['watchers']
     end
 
     def stats
       return @stats if @stats
 
-      @stats = Hash.new
-      [ 'contributors',
-        'commit_activity',
-        'code_frequency',
-        'participation',
-        'punch_card'].each do |stat|
-          repo_stats_response = github_api_get('/' + @full_name + '/stats/' + stat)
-          @stats[stat] = JSON.load(repo_stats_response.to_s)
+      @stats = {}
+      %w(
+        contributors commit_activity code_frequency participation punch_card
+      ).each do |stat|
+        @stats[stat] = @github_api.repo_stat(@full_name, stat)
       end
     end
-
   end
 end
-
-# https://developer.github.com/v3/repos/statistics/
-
-# File.write('github_response.yml', github_response.to_yaml)
-# File.write('results.yml', results.to_yaml)
